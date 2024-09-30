0bc40cd37b2ac4a8d2d51c802b781b3493bec894
scroohas <121280716+scroohas@users.noreply.github.com>
2024-09-30 09:23:45 +0000

    added consoles

--- index.js
+++ index.js
@@ -11,18 +11,7 @@ app.use(bodyParser.json());
 app.post('/api/variables', (req, res) => {
     const variables = req.body;
 
-    if (!Array.isArray(variables) || variables.length === 0) {
-        return res.status(400).json({ message: "Invalid input. An array of variables is required." });
-    }
-
-    // Validate the input
-    const isValid = variables.every(variable => {
-        return variable.variable_name && variable.datetime && variable.value && variable.variableId;
-    });
-
-    if (!isValid) {
-        return res.status(400).json({ message: "Invalid data. Each object must have variable_name, datetime, value, and variableId." });
-    }
+    console.log(variables)
 
     // Respond back with the received data
     return res.status(200).json({
